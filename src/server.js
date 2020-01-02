f66fcd30cb905d5fe0c8c83d23c4df86628cc5c0
dannyvidal <vidaldanny@outlook.com>
2020-01-02 18:54:24 -0500

    added cors

--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
 const PORT = process.env.PORT || 5000;
 var express = require("express");
+var cors = require("cors");
 var accountRouter = require("./Routes/AccountRouter.js");
 var app = express();
+app.use(cors());
 
 //updateable local variables for accountRouter
 app.locals.updateableFields = ["email", "password", "firstName", "lastName", "workHistory", "city", "state", "skills", "bio","interviewer", "references"];
