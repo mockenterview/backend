0414b82172a9bc08e8dfa4de67f127cba30002fd
dannyvidal <vidaldanny@outlook.com>
2020-01-02 13:49:42 -0500

    adde register component

--- src/Routes/AccountRouter.js
+++ src/Routes/AccountRouter.js
@@ -1,22 +1,24 @@
 var accountRouter = require("express").Router();
 var AccountModel = require("../Models/Account.js");
 var parseMongooseError = require("./utils/parseMongooseError.js");
+var bodyCheck = require("./utils/bodycheck.js");
 
 accountRouter.route("/register")
-    .post(async ({body:{email, password}}, response) => {
-       
-        if(!email || !password){
-            response.status(400).json({message:"Bad request body needs to contain email/password"});
-        }else{
-            let account = {email, password};
-            try{
-                
+    .post(async (request, response) => {
+        
+        let body = bodyCheck(Object.keys(request.body), ["email", "password", "firstName", "lastName"]);
+        
+        if(body.valid){
+            let account = {email, password, firstName, lastName} = request.body
+        
+            try{       
                 await AccountModel.create(account);
-                
                 response.status(201).json({message:"Account created"});
             }catch(err){
                 response.status(500).json({message:parseMongooseError(err)});
             }
+        }else{
+            response.status(400).json({message:body.message});
         }
     })
 module.exports = accountRouter;
