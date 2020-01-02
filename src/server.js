6f1f84fc5807d9d36f4a169047a736e7f8f4e6ac
dannyvidal <vidaldanny@outlook.com>
2020-01-02 13:18:11 -0500

    included accountRouter

--- src/server.js
+++ src/server.js
@@ -1,7 +1,12 @@
 const PORT = process.env.PORT || 5000;
 var express = require("express");
+var accountRouter = require("./Routes/AccountRouter.js");
 var app = express();
 
-app.listen(() => {
+app.use(express.json());
+app.use("/api", accountRouter);
+
+
+app.listen(PORT, () => {
     console.log(`App up and running on port ${PORT}`);
 })
