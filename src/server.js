533f8663c37dd08f60f5939768c84a3f214fb8fe
dannyvidal <vidaldanny@outlook.com>
2020-01-02 16:49:04 -0500

    added updateable fields

--- src/server.js
+++ src/server.js
@@ -3,6 +3,9 @@ var express = require("express");
 var accountRouter = require("./Routes/AccountRouter.js");
 var app = express();
 
+//updateable local variables for accountRouter
+app.locals.updateableFields = ["email", "password", "firstName", "lastName", "workHistory", "city", "state", "skills", "bio","interviewer", "references"];
+
 app.use(express.json());
 app.use("/api", accountRouter);
 
