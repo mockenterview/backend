a35919d772c9c6de3ee5850bb7443457a4f161b6
dannyvidal <vidaldanny@outlook.com>
2020-01-02 14:33:23 -0500

    'promisified' function

--- src/Routes/utils/bodycheck.js
+++ src/Routes/utils/bodycheck.js
@@ -1,21 +1,23 @@
-//predicate function returns true if keys exist in body object if false with what is missing
+//predicate function promise  always resolves object valid:true if keys exist in body; object valid:false if false with msg of what is missing
 module.exports = function bodyCheck(bodyKeys, requiredKeys){
-    let missing = [];
-    for(let key of requiredKeys){
-        if(!bodyKeys.includes(key)){
-            missing.push(key);
-        }
-    }
-    if(missing.length > 0){
-        return {valid:false, message:(() => {
-            let msg = "Bad Request body is missing ";
-            for(let x of missing){
-                msg += `${x}/`
+   return new Promise((res) => {
+        let missing = [];
+        for(let key of requiredKeys){
+            if(!bodyKeys.includes(key)){
+                missing.push(key);
             }
-            return msg.substring(0, msg.length - 1);
-        })()};
-    }else{
-        return {valid:true};
-    }
+        }
+        if(missing.length > 0){
+            res({valid:false, message:(() => {
+                let msg = "Bad Request body is missing ";
+                for(let x of missing){
+                    msg += `${x}/`
+                }
+                return msg.substring(0, msg.length - 1);
+            })()});
+        }else{
+            res({valid:true});
+        }
+   })
     
 }
